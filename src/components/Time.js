db1cd544ab000a40e0e75b3d77de422843a8dc59
Nirav <niravpatelp129@gmail.com>
2024-08-05 14:18:19 -0400

    * feat(components): improved time tracking

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from 'react';
-const { ipcRenderer } = window.require('electron');
 
 export default function Time() {
   const [time, setTime] = useState('00:00:00');
   const [isRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
 
   useEffect(() => {
     let interval;
     if (isRunning) {
       interval = setInterval(() => {
-        ipcRenderer.send('update-timer');
+        const now = Date.now();
+        const elapsed = now - startTime + elapsedTime;
+        const seconds = Math.floor(elapsed / 1000) % 60;
+        const minutes = Math.floor(elapsed / 60000) % 60;
+        const hours = Math.floor(elapsed / 3600000);
+        setTime(
+          `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
+            .toString()
+            .padStart(2, '0')}`,
+        );
       }, 1000);
-
-      ipcRenderer.on('timer-updated', (event, newTime) => {
-        setTime(newTime);
-      });
     }
-    return () => {
-      clearInterval(interval);
-      ipcRenderer.removeAllListeners('timer-updated');
-    };
-  }, [isRunning]);
+    return () => clearInterval(interval);
+  }, [isRunning, startTime, elapsedTime]);
 
   const toggleTimer = () => {
-    setIsRunning(!isRunning);
-    ipcRenderer.send('toggle-timer');
+    if (isRunning) {
+      setElapsedTime((prevElapsedTime) => prevElapsedTime + Date.now() - startTime);
+    } else {
+      setStartTime(Date.now());
+    }
+    setIsRunning((prevIsRunning) => !prevIsRunning);
   };
 
   return (
