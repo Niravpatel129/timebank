15c8d2e3de32b5322f2d28727b9925e53ad6e0b1
Nirav <niravpatelp129@gmail.com>
2024-08-11 16:01:44 -0400

    * refactor(index.js): removed unnecessary line

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Toaster />
-
     <Router>
       <ProjectProvider>
         <ScreenProvider>
