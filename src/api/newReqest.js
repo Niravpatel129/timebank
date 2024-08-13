21a0e204f8467017e7952c28c13ec2787959e24c
Nirav <niravpatelp129@gmail.com>
2024-08-13 12:02:23 -0400

    * refactor(api): simplified base URL

--- src/api/newReqest.js
+++ src/api/newReqest.js
@@ -9,10 +9,7 @@ const makeRequest = async (method, url, data = null, options = {}) => {
       ...options.headers,
     };
 
-    const baseUrl =
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:8004'
-        : 'https://timebank-305bb7cb7d96.herokuapp.com';
+    const baseUrl = 'https://timebank-305bb7cb7d96.herokuapp.com';
 
     const requestOptions = {
       method,
